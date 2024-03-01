12cc69e8b0b86a62012f6f9755a7de9c3cdf508b
MonPrado <104338049+MonPrado@users.noreply.github.com>
2024-02-29 22:12:03 -0300

    Site Atualizado

--- captura-valor.js
+++ captura-valor.js
@@ -1,3 +1,6 @@
+var titulo = document.querySelector(".titulo_secao");
+titulo.textContent = "Lista de Encomendas";
+
 //Captura todos os clientes que fizeram encomendas 
 var clientes = document.querySelectorAll(".linhas");
 
@@ -5,17 +8,19 @@ var clientes = document.querySelectorAll(".linhas");
 for (var count = 0; count < clientes.length; count++) {
 
     //Captura a quantidade encomendada
-    var qtde = clientes[count].querySelector(".info-qtde").textContent;
+    var qtde = parseInt(clientes[count].querySelector(".info-qtde").textContent);
     //Captura o valor unitário do produto 
-    var unitario = clientes[count].querySelector(".info-valor").textContent;
+    var unitario = parseFloat(clientes[count].querySelector(".info-valor").textContent);
 
     //Valida a quantidade 
-    if (qtde < 1 || isNaN(qtde)) {
+    if (qtde <= 0 || isNaN(qtde)) {
         // Quando NOK, avisa o usuário
         clientes[count].querySelector(".info-qtde").textContent = "Quantidade Inválida";
         clientes[count].querySelector(".info-qtde").style.color = "red";
+        clientes[count].querySelector(".info-valor").textContent = formaValorMonetario(unitario);
     } else {
-        if (isNaN(unitario)) {
+        //Quantidade OK --- Prosseguir
+        if (unitario <= 0 || isNaN(unitario)) {
             // Quando NOK, avisa o usuário
             clientes[count].querySelector(".info-valor").textContent = "Valor Inválido";
             clientes[count].style.backgroundColor = "red";
@@ -23,18 +28,23 @@ for (var count = 0; count < clientes.length; count++) {
         } else {
             //Quantidade ok, prossegue
             //Calcula o valor total da encomenda 
-            clientes[count].querySelector(".info-valor-total").textContent = "R$ " + calculoTotal(qtde,unitario);
-           
+            clientes[count].querySelector(".info-valor").textContent = formaValorMonetario(unitario);
+            clientes[count].querySelector(".info-valor-total").textContent = formaValorMonetario(calculoTotal(qtde, unitario));
         }
     }
 
+    function calculoTotal(qtde, unitario) {
+        var total = 0;
+        total = qtde * unitario;
+
+        return total;
+    }
 
 }
 
 //Função para calcular o valor total de encomendas 
-function calculoTotal(qtde, unitario) {
-    var total = 0;
-    total = qtde * unitario;
-
-    return total;
+function formaValorMonetario(valor) {
+    var formReal = 0;
+    formReal = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return formReal;
 } 
